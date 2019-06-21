c4cb7934be261e0664758581ee5eb70b79fbcf7a
huyh <huyh@51kezhi.com>
2019-06-21 16:32:11 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <div class="demos">台况信息</div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  color:  red;
+
+}
+.demos {
+  width: 100%;
+  font-size: rem(19);
+  background: #fff;
+  height: rem(46);
+  display: flex;
+  align-items: center;
+  color: #ff8815;
+  justify-content: center;
+  @include border($type: 1);
+  
+  
+ 
+ 
+}
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+const path = require('path')
+
+module.exports = {
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      // 需要通过less-loader自动引入的资源，集合类型
+      patterns: [path.resolve(__dirname, './src/common/commom.scss')]
+    }
+  }
+}
